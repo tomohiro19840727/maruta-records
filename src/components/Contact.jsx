690f97857479f79c443c784808e6dc58e691a2eb
tomohiro <tomohirofarm@gmail.com>
2023-06-24 19:49:34 +0900

    13

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -54,7 +54,7 @@ const Contact = () => {
           <label for="first-name" class="mb-2 inline-block text-sm text-gray-800 sm:text-base">name</label>
           <input 
           type="text"
-          placeholder="山田"
+          placeholder="田中"
           value={name}
           onChange={(e) => setName(e.target.value)}
          class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
