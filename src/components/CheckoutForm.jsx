61a40930e1451ff8c80ac2718e92b2d1c88ba638
tomohiro <tomohirofarm@gmail.com>
2023-07-17 10:13:47 +0900

    53

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -5,7 +5,7 @@ import {
   useStripe,
   useElements
 } from "@stripe/react-stripe-js";
-import { Link } from "react-router-dom";
+
 
 export default function CheckoutForm() {
   const stripe = useStripe();
