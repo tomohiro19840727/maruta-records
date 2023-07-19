e34008c6a2c53e5495833f345dc230c76d00a974
tomohiro <tomohirofarm@gmail.com>
2023-07-19 14:29:24 +0900

    12

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -62,7 +62,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/",
+        return_url: "https://maruta-records.vercel.app/checkout",
       },
     });
 
